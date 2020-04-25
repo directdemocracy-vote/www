95acd174dc364b1343da4124f1af8aa73f41a4ec
Galine <webmaster@cyberbotics.com>
2020-04-25 12:32:12 +0200

    Framework7 version

---
+++ httpdocs/js/f7.js
@@ -0,0 +1,17 @@
+var app = new Framework7({
+  root: '#app',
+  name: 'directdemocracy.vote',
+  id: 'directdemocracy.vote',
+  routes: [
+    // Add your routes here
+    // Example:
+    /*
+    {
+      path: '/about/',
+      url: 'about.html',
+    },
+    */
+  ],
+});
+
+var mainView = app.views.create('.view-main');
