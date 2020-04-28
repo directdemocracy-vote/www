70928f4b27c850133e64da3034df5775f69200c6
Galine <webmaster@cyberbotics.com>
2020-04-28 22:33:19 +0200

    progress

--- httpdocs/js/f7.js
+++ httpdocs/js/f7.js
@@ -10,5 +10,48 @@ var app = new Framework7({
 var mainView = app.views.create('.view-main');
 
 window.onload = function() {
+  let croppie = null;
+  let citizen = {};
+
+  function uploadPicture() {
+    document.getElementById('register-picture-upload').click();
+  }
+  document.getElementById('register-upload-button').addEventListener('click', uploadPicture);
+  document.getElementById('register-picture').addEventListener('click', uploadPicture);
+
+  document.getElementById('register-picture-upload').addEventListener('change', function(event) {
+    if (croppie) croppie.destroy();
+    let img = document.getElementById('register-picture');
+    img.src = URL.createObjectURL(event.target.files[0]);
+    event.target.value = '';
+    croppie = new Croppie(img, {
+      boundary: {
+        width: 250,
+        height: 300
+      },
+      viewport: {
+        width: 150,
+        height: 200
+      },
+      enableExif: true
+    });
+    document.getElementById('picture-select').style.display = '';
+  });
+
+  document.getElementById('picture-select').addEventListener('click', function() {
+    croppie.result({
+      type: 'base64',
+      format: 'jpeg',
+      quality: 0.95
+    }).then(function(result) {
+      document.getElementById('register-picture').setAttribute('src', result);
+      citizen.picture = result;
+      document.getElementById('picture-select').style.display = 'none';
+      croppie.destroy();
+      croppie = null;
+      // validate();
+    });
+    return false;
+  });
 
 };
