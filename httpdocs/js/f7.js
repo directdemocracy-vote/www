06bd26cfd2b9b105248483796a5f5b118e5fa803
Galine <webmaster@cyberbotics.com>
2020-04-29 22:07:17 +0200

    update

--- httpdocs/js/f7.js
+++ httpdocs/js/f7.js
@@ -1,3 +1,4 @@
+//import 'https://cdn.jsdelivr.net/npm/exif-js';
 import QrScanner from './qr-scanner.min.js';
 QrScanner.WORKER_PATH = 'js/qr-scanner-worker.min.js';
 
@@ -21,37 +22,76 @@ window.onload = function() {
 
   document.getElementById('register-picture-upload').addEventListener('change', function(event) {
     if (croppie) croppie.destroy();
-    let img = document.getElementById('register-picture');
+    let content = {};
+    content.innerHTML =
+      `<div class="sheet-modal" style="height: 100%">
+  <div class="toolbar">
+    <div class="toolbar-inner">
+      <div class="left" style="margin-left:16px">Adjust your ID photo</div>
+      <div class="right">
+        <a href="#" class="link sheet-close">Done</a>
+      </div>
+    </div>
+  </div>
+  <div class="sheet-modal-inner">
+    <div class="block">
+      <p><img id="edit-picture"></p>
+      <div class="row">
+        <button class="col button" id="rotate-right"><i class="icon f7-icons">rotate_right_fill</i></button>
+        <button class="col button" id="rotate-left"><i class="icon f7-icons">rotate_left_fill</i></button>
+      </div>
+    </div>
+  </div>
+</div>`;
+    var sheet = app.sheet.create({
+      content: content.innerHTML,
+      on: {
+        opened: function() {
+          console.log('Sheet opened');
+        }
+      }
+    });
+    sheet.open();
+    let img = document.getElementById('edit-picture');
     img.src = URL.createObjectURL(event.target.files[0]);
     event.target.value = '';
+    let w = screen.width * 0.8;
     croppie = new Croppie(img, {
       boundary: {
-        width: 250,
-        height: 300
+        width: w,
+        height: w * 1.5
       },
       viewport: {
-        width: 150,
-        height: 200
+        width: w * 0.75,
+        height: w * 0.75 * 1.5
       },
+      enableOrientation: true,
       enableExif: true
     });
-    document.getElementById('picture-select').style.display = '';
-  });
-
-  document.getElementById('picture-select').addEventListener('click', function() {
-    croppie.result({
-      type: 'base64',
-      format: 'jpeg',
-      quality: 0.95
-    }).then(function(result) {
-      document.getElementById('register-picture').setAttribute('src', result);
-      citizen.picture = result;
-      document.getElementById('picture-select').style.display = 'none';
-      croppie.destroy();
-      croppie = null;
-      // validate();
+    document.getElementById('rotate-right').addEventListener('click', function() {
+      croppie.rotate(-90);
     });
-    return false;
-  });
+    document.getElementById('rotate-left').addEventListener('click', function() {
+      croppie.rotate(90);
+    });
 
+    // document.getElementById('picture-select').style.display = '';
+  });
+  /*
+    document.getElementById('picture-select').addEventListener('click', function() {
+      croppie.result({
+        type: 'base64',
+        format: 'jpeg',
+        quality: 0.95
+      }).then(function(result) {
+        document.getElementById('register-picture').setAttribute('src', result);
+        citizen.picture = result;
+        document.getElementById('picture-select').style.display = 'none';
+        croppie.destroy();
+        croppie = null;
+        // validate();
+      });
+      return false;
+    });
+  */
 };
