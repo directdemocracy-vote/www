627ac8c300f1b21a35936b7f64c6df8801641293
Galine <webmaster@cyberbotics.com>
2020-05-02 11:38:20 +0200

    map

--- httpdocs/js/f7.js
+++ httpdocs/js/f7.js
@@ -1,4 +1,3 @@
-//import 'https://cdn.jsdelivr.net/npm/exif-js';
 import QrScanner from './qr-scanner.min.js';
 QrScanner.WORKER_PATH = 'js/qr-scanner-worker.min.js';
 
@@ -11,7 +10,6 @@ var app = new Framework7({
 var mainView = app.views.create('.view-main');
 
 window.onload = function() {
-  let croppie = null;
   let citizen = {};
 
   function uploadPicture() {
@@ -21,7 +19,6 @@ window.onload = function() {
   document.getElementById('register-picture').addEventListener('click', uploadPicture);
 
   document.getElementById('register-picture-upload').addEventListener('change', function(event) {
-    if (croppie) croppie.destroy();
     let content = {};
     content.innerHTML =
       `<div class="sheet-modal" style="height: 100%">
@@ -43,14 +40,35 @@ window.onload = function() {
     </div>
   </div>
 </div>`;
+    let croppie = null;
     let sheet = app.sheet.create({
       content: content.innerHTML,
       on: {
         opened: function() {
-          console.log('Sheet opened');
+          let img = document.getElementById('edit-picture');
+          img.src = URL.createObjectURL(event.target.files[0]);
+          event.target.value = '';
+          let w = screen.width * 0.95;
+          croppie = new Croppie(img, {
+            boundary: {
+              width: w,
+              height: w * 4 / 3
+            },
+            viewport: {
+              width: w * 0.75,
+              height: w * 0.75 * 4 / 3
+            },
+            enableOrientation: true,
+            enableExif: true
+          });
+          document.getElementById('rotate-right').addEventListener('click', function() {
+            croppie.rotate(-90);
+          });
+          document.getElementById('rotate-left').addEventListener('click', function() {
+            croppie.rotate(90);
+          });
         },
         close: function() {
-          console.log('Sheet closing');
           croppie.result({
             type: 'base64',
             format: 'jpeg',
@@ -65,27 +83,106 @@ window.onload = function() {
       }
     });
     sheet.open();
-    let img = document.getElementById('edit-picture');
-    img.src = URL.createObjectURL(event.target.files[0]);
-    event.target.value = '';
-    let w = screen.width * 0.95;
-    croppie = new Croppie(img, {
-      boundary: {
-        width: w,
-        height: w * 4 / 3
-      },
-      viewport: {
-        width: w * 0.75,
-        height: w * 0.75 * 4 / 3
-      },
-      enableOrientation: true,
-      enableExif: true
-    });
-    document.getElementById('rotate-right').addEventListener('click', function() {
-      croppie.rotate(-90);
-    });
-    document.getElementById('rotate-left').addEventListener('click', function() {
-      croppie.rotate(90);
-    });
   });
+  document.getElementById('register-location-button').addEventListener('click', function() {
+    let content = {};
+    content.innerHTML =
+      `<div class="sheet-modal" style="height: 100%">
+  <div class="toolbar">
+    <div class="toolbar-inner">
+      <div class="left" style="margin-left:16px">Select your home location</div>
+      <div class="right">
+        <a href="#" class="link sheet-close">Done</a>
+      </div>
+    </div>
+  </div>
+  <div class="sheet-modal-inner">
+    <div class="block margin-top-half no-padding-left no-padding-right">
+      <p><small class="form-text text-muted">Zoom to building level to precisely select your home address.</small></p>
+      <div id="register-map" style="width:100%;height:500px;margin-top:10px"></div>
+    </div>
+  </div>
+</div>`;
+    let sheet = app.sheet.create({
+      content: content.innerHTML,
+      on: {
+        opened: function() {
+          console.log('Sheet opened');
+          let geolocation = false;
+
+          function updateLocation() {
+            register_marker.setPopupContent(citizen.latitude + ', ' + citizen.longitude).openPopup();
+            let xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+              if (this.readyState == 4 && this.status == 200) {
+                const a = JSON.parse(this.responseText);
+                const address = a.display_name;
+                register_marker.setPopupContent(address + '<br><br><center style="color:#999">(' +
+                  citizen.latitude + ', ' + citizen.longitude + ')</center>').openPopup();
+              }
+            };
+            xhttp.open('GET', 'https://nominatim.openstreetmap.org/reverse.php?format=json&lat=' + citizen.latitude +
+              '&lon=' +
+              citizen.longitude + '&zoom=20', true);
+            xhttp.send();
+          }
+
+          function getGeolocationPosition(position) {
+            geolocation = true;
+            citizen.latitude = round_geo(position.coords.latitude);
+            citizen.longitude = round_geo(position.coords.longitude);
+            register_map.setView([citizen.latitude, citizen.longitude], 12);
+            setTimeout(function() {
+              register_marker.setLatLng([citizen.latitude, citizen.longitude]);
+              updateLocation();
+            }, 500);
+          }
+
+          function round_geo(v) {
+            return Math.round(v * 1000000) / 1000000;
+          }
+          if (navigator.geolocation)
+            navigator.geolocation.getCurrentPosition(getGeolocationPosition);
+          let xhttp = new XMLHttpRequest();
+          xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200 && geolocation == false) {
+              coords = this.responseText.split(',');
+              getGeolocationPosition({
+                coords: {
+                  latitude: coords[0],
+                  longitude: coords[1]
+                }
+              });
+            }
+          };
+          xhttp.open('GET', 'https://ipinfo.io/loc', true);
+          xhttp.send();
+          citizen.latitude = 0;
+          citizen.longitude = 0;
+          let register_map = L.map('register-map').setView([citizen.latitude, citizen.longitude], 2);
+          L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a>'
+          }).addTo(register_map);
+          let register_marker = L.marker([citizen.latitude, citizen.longitude]).addTo(register_map)
+            .bindPopup(citizen.latitude + ',' + citizen.longitude);
+          updateLocation();
+          register_map.on('contextmenu', function(event) {
+            return false;
+          });
+          register_map.on('click', function onMapClick(e) {
+            citizen.latitude = round_geo(e.latlng.lat);
+            citizen.longitude = round_geo(e.latlng.lng);
+            register_marker.setLatLng([citizen.latitude, citizen.longitude]);
+            updateLocation();
+          });
+        },
+        close: function() {
+          console.log('Sheet closing');
+          document.getElementById('register-location').value = citizen.latitude + ', ' + citizen.longitude;
+        }
+      }
+    });
+    sheet.open();
+  });
+
 };
