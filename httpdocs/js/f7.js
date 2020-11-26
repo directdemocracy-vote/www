78f853a07f09ca22ca81d3237e66490c89376384
Megatol <license@cyberbotics.com>
2020-11-26 11:00:40 +0100

    fix

--- httpdocs/js/f7.js
+++ httpdocs/js/f7.js
@@ -751,8 +751,7 @@ window.onload = function() {
         this.style.marginLeft = margin + '%';
         this.style.marginRight = margin + '%';
       } else {
-        // const margin = -130 * (this.videoHeight - this.videoWidth) / this.videoHeight;
-        const margin = -84;
+        const margin = -100 * (3 * this.videoHeight + this.videoWidth) / (4 * this.videoHeight);
         message.innerHTML = "video = " + this.videoWidth + " x " + this.videoHeight + ' margin: ' + margin + '%';
         this.style.width = '100%';
         this.style.marginTop = margin + '%';
