0d6386c74f1ee3e84a3b6a86396ee8d1912c2096
Megatol <license@cyberbotics.com>
2022-01-10 21:04:25 +0100

    fix

--- httpdocs/js/f7.js
+++ httpdocs/js/f7.js
@@ -1629,8 +1629,9 @@ window.onload = function() {
       disable(button);
       disableAnswer(index, true);
     } else if (document.querySelector('input[name="answer-' + index + '"]:checked')) {
-      // button.innerHTML = '<i class="icon f7-icons">envelope</i> Vote';
-      button.innerHTML = '<i class="icon f7-icons" style="transform:rotate(90deg)">hand_point_right</i> Vote';
+      // button.innerHTML = '<i class="icon f7-icons" style="margin-right:10px">envelope</i> Vote';
+      button.innerHTML =
+        '<i class="icon f7-icons" style="margin-right:10px;transform:rotate(90deg)">hand_point_left</i> Vote';
       message.innerHTML = 'Think twice before you vote, there is no undo.';
       enable(button);
     } else {
