26c26a713821db14b551afcc95c268e78b4b73be
Olivier Michel <Olivier.Michel@cyberbotics.com>
2023-11-18 19:05:36 +0100

    translation

--- httpdocs/js/index.js
+++ httpdocs/js/index.js
@@ -1,4 +1,28 @@
+import Translator from 'https://app.directdemocracy.vote/app/js/translator.js';
+
 document.addEventListener('DOMContentLoaded', () => {
+  console.log(navigator.language);
+  let translator = new Translator('i18n');
+  translator.onready = function() {
+    console.log(translator.language);
+    console.log(translator.languages[translator.language]);
+    for (const [key, value] of Object.entries(translator.languages)) {
+      document.getElementById(`language-${key}`).addEventListener('click', function(event) {
+
+      });
+      console.log(key, value);
+    }
+    if (translator.language === 'en')
+      return; // default, already set
+    let flag = '🇺🇸';
+    switch (translator.language) {
+      case 'fr': flag = '🇫🇷'; break;
+      default: flag = '?';
+    }
+    document.getElementById('language-en').classList.remove('is-hidden');
+    document.getElementById(`language-${translator.language}`).classList.add('is-hidden');
+    document.getElementById('language').innerHTML = flag;
+  };
   const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
   $navbarBurgers.forEach(el => {
     el.addEventListener('click', () => {
