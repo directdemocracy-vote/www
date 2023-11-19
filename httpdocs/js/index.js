cab2b45753de77445f95f21e1e8f58b97bda9cb4
Olivier Michel <Olivier.Michel@cyberbotics.com>
2023-11-19 22:25:14 +0100

    Language menu

--- httpdocs/js/index.js
+++ httpdocs/js/index.js
@@ -1,27 +1,35 @@
 import Translator from 'https://app.directdemocracy.vote/app/js/translator.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  console.log(navigator.language);
   let translator = new Translator('i18n');
-  translator.onready = function() {
-    console.log(translator.language);
-    console.log(translator.languages[translator.language]);
-    for (const [key, value] of Object.entries(translator.languages)) {
-      document.getElementById(`language-${key}`).addEventListener('click', function(event) {
-
-      });
-      console.log(key, value);
+  function setLanguage(language, previous) {
+    if (previous === undefined) {
+      previous = translator.language;
+      const dd = document.getElementById('language-dropdown');
+      dd.classList.add('is-hidden');
+      setTimeout(() => {
+        dd.classList.remove('is-hidden');
+      }, 100);
     }
-    if (translator.language === 'en')
-      return; // default, already set
-    let flag = '🇺🇸';
-    switch (translator.language) {
+    translator.language = language;
+    let flag;
+    switch (language) {
+      case 'en': flag = '🇺🇸'; break;
       case 'fr': flag = '🇫🇷'; break;
       default: flag = '?';
     }
-    document.getElementById('language-en').classList.remove('is-hidden');
-    document.getElementById(`language-${translator.language}`).classList.add('is-hidden');
+    document.getElementById(`language-${previous}`).classList.remove('is-disabled');
+    document.getElementById(`language-${language}`).classList.add('is-disabled');
     document.getElementById('language').innerHTML = flag;
+  }
+  translator.onready = function() {
+    for (const [key] of Object.entries(translator.languages)) {
+      document.getElementById(`language-${key}`).addEventListener('click', function(event) {
+        setLanguage(key);
+      });
+    }
+    if (translator.language !== 'en') // default one
+      setLanguage(translator.language, 'en');
   };
   const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
   $navbarBurgers.forEach(el => {
