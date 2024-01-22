99f67870936efd1c8fee5a31f61ac5ec7b89eb72
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-01-22 16:59:56 +0100

    Update checkout.php

--- httpdocs/stripe/checkout.php
+++ httpdocs/stripe/checkout.php
@@ -16,8 +16,9 @@ $checkout_session = $stripe->checkout->sessions->create([
   ]],
   'mode' => 'subscription',
   # 'submit_type' => 'donate',
-  'cancel_url' => 'https://directdemocracy.vote',
-  'return_url' => $YOUR_DOMAIN . '/stripe/return.html?session_id={CHECKOUT_SESSION_ID}'
+  'success_url' => 'https://directdemocracy.vote/success',
+  'cancel_url' => 'https://directdemocracy.vote'
+  # 'return_url' => $YOUR_DOMAIN . '/stripe/return.html?session_id={CHECKOUT_SESSION_ID}'
 ]);
 
 echo json_encode(array('clientSecret' => $checkout_session->client_secret));
