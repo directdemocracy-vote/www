8d526ba8744a529fcb8409c11c596b2051acbe61
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-01-22 17:11:35 +0100

    Update checkout.js

--- httpdocs/js/checkout.js
+++ httpdocs/js/checkout.js
@@ -1,6 +1,7 @@
 window.addEventListener("load", function() {
   document.getElementById('donate-button').addEventListener('click', async function(event) {
-    event.currentTarget.classList.add('is-loading');
+    const button = event.currentTarget;
+    button.classList.add('is-loading');
     const stripe = Stripe('pk_test_51ONAiHJ8bitZPVQT83mvU9hsFgAcXYctJa6wFynuQ7ZieWQHLeFmmdNlJMpECaIkVz87vBHnbBgW9q48qc9fdvcr00oudVLpYM');
     const response = await fetch('/stripe/checkout.php', {method: 'POST'});
     const {clientSecret} = await response.json();
@@ -10,7 +11,7 @@ window.addEventListener("load", function() {
     document.getElementById('donate-form').classList.add('is-hidden');
     document.getElementById('donate-checkout').classList.remove('is-hidden');
     document.getElementById('donate-back').classList.remove('is-hidden');
-    event.currentTarget.classList.remove('is-loading');
+    button.classList.remove('is-loading');
   });
   document.getElementById('donate-back').addEventListener('click', async function(event) {
     document.getElementById('donate-explanation').classList.remove('is-hidden');
