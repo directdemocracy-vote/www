3a132aa1a9d94095ec24820472c1fe452c2d9a42
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-01-23 15:03:26 +0100

    Update checkout.php

--- httpdocs/stripe/checkout.php
+++ httpdocs/stripe/checkout.php
@@ -134,4 +134,4 @@ $checkout_session = $stripe->checkout->sessions->create([
   'redirect_on_completion' => 'never'
 ]);
 
-echo json_encode(array('clientSecret' => $checkout_session->client_secret));
+echo json_encode(array('clientSecret' => $checkout_session->client_secret, 'clientId' => 0));
