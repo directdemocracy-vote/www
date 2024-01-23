68f6bc5219c9c13e1a1e1ebb4b8b6c482862cd93
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-01-23 15:29:36 +0100

    Update checkout.js

--- httpdocs/js/checkout.js
+++ httpdocs/js/checkout.js
@@ -6,9 +6,8 @@ window.addEventListener("load", function() {
   const amounts = [5, 10, 20, 50, 100, 200, 500, 1000];
   let isOrganization = false;
   let display = false;
-  document.getElementById('donate-form').addEventListener('submit', async function(event) {
-    event.preventDefault();
-    const button = document.getElementById('donate-button');
+  document.getElementById('donate-button').addEventListener('click', async function(event) {
+    const button = event.currentTarget;
     button.classList.add('is-loading');
     const stripe = Stripe('pk_test_51ONAiHJ8bitZPVQT83mvU9hsFgAcXYctJa6wFynuQ7ZieWQHLeFmmdNlJMpECaIkVz87vBHnbBgW9q48qc9fdvcr00oudVLpYM');
     const currency = document.querySelectorAll('input[name="donation-currency"]:checked')[0].value;
