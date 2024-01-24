b53ba636937e6ce8959be8df3fd556677a239f90
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-01-24 09:30:01 +0100

    Update terms_of_service.php

--- httpdocs/terms_of_service.php
+++ httpdocs/terms_of_service.php
@@ -42,39 +42,38 @@ For the purposes of these Terms of Service:
 </p>
 <h2 class="title is-2">Collecting and Using Your Personal Data</h2>
 
-<h3 class="title is-3">Types of Data Collected</h3>
+<h3 class="title is-3">Types of Personal Data Collected</h3>
 
-<h4 class="title is-4">Personal Data</h4>
-<p>
-While donating to the Us, We may ask You to provide Us with certain personally identifiable information that can be used to identify You.
-Personally identifiable information may include, but is not limited to:
+<p>While donating to the Us, We may ask You to provide Us with certain personally identifiable information that can be used to identify You.
+Personally identifiable information may include, but is not limited to:</p>
 <ul>
 <li>Family name</li>
 <li>Given names</li>
 <li>Organization name</li>
 <li>E-mail address</li>
 <li>Amounts paid</li>
-<li>Currency used for the payments</li>
+<li>Currencies used for the payments</li>
 <li>Comments</li>
+<li>Payment dates</li>
 </ul>
-You may also provide Our Third Party Payment Services with additional information, such as:
+<p>You may also provide Our Third Party Payment Services with additional information, such as:</p>
 <ul>
 <li>Credit card information</li>
 <li>Other payment information</li>
 </ul>
-</p>
-<h4 class="title is-4">Usage Data</h4>
-<p>
-Usage Data is collected automatically by Third Party Payment Services when donating to Us.
-We strongly recommend that you review the Terms of Service of Our Third Party Payment Services to understand how your Personal Data will be used, transfered, disclosed and stored securely.
-</p>
-<h3 class="title is-3">Retention, Use, Transfer, Disclosure and Security of Your Personal Data and Usage Data</h3>
+<h3 class="title is-3">Retention, Use, Transfer, Disclosure and Security of Your Personal Data</h3>
 <p>Your Personal Data is stored securely in our databases and won't be transferred or disclosed to any third party without your explicit consent.</p>
 <p>You may request Us anytime to delete your Personal Data from our databases.</p>
-<p>We may use your Personal Data to create anonymized statistical data which could be used for Our own purposes.</p>
-<p>We may occasionally send you some e-mail to inform you about the receipt of your payment, changes in these Terms of Services and other important information.</p>
-<p>We will never send you unsollicited advertising.</p>
+<p>Personal Data is collected automatically by Third Party Payment Services when donating to Us.
+We strongly recommend that you review the Terms of Service of Our Third Party Payment Services to understand how your Personal Data will be used, transfered, disclosed and stored securely.</p>
 
+<h3 class="title is-3">Personal Data Usage</h4>
+<p>
+We may use your Personal Data to create anonymized statistical data which could be used for Our own purposes.
+We may occasionally send you some e-mail to inform you about the receipt of your payment, changes in these Terms of Services and other important information.
+We will never send you unsollicited advertising.
+</p>
+  
 <h2 class="title is-2">Children's Privacy</h2>
 <p>
 Our Service does not address anyone under the age of 13.
