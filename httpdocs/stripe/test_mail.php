c93a0f776c2d82c7b2bfb37c68916ecbb4d373a9
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-01-24 11:22:50 +0100

    Update test_mail.php

--- httpdocs/stripe/test_mail.php
+++ httpdocs/stripe/test_mail.php
@@ -1,5 +1,5 @@
 <?php
 
-mail('Olivier.Michel', 'This is a test', 'Yes', 'From: info@directdemocracy.vote');
+mail('Olivier.Michel@cyberbotics.com', 'This is a test', 'Yes', 'From: info@directdemocracy.vote');
 
 ?>
