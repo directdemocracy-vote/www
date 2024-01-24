af0af564eeacd0e7a1377313f7188506cfccf885
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-01-24 09:13:28 +0100

    Create terms_of_service.php

---
+++ httpdocs/terms_of_service.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Terms of Service</title>
+  <link rel="icon" href="/favicon.svg" type="image/svg+xml" sizes="any">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+  <link rel="stylesheet" href="https://directdemocracy.vote/css/directdemocracy.css">
+  <link rel="stylesheet" href="https://directdemocracy.vote/css/corner-ribbon.css">
+</head>
+<body>
+<div class="content">
+<section class="section">
+
+<h1 class="title is-1">Terms of Service</h1>
+
+<p><em>Last updated: January 24, 2024</em></p>
+
+<p>
+These Terms of Service describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.
+We use Your Personal data to provide the Service. By using the Service, You agree to the collection and use of information in accordance with these Terms of Service.
+</p>
+
+<h2 class="title is-2">Interpretation and Definitions</h2>
+
+<h3 class="title is-3">Interpretation</h3>
+<p>
+The words of which the initial letter is capitalized have meanings defined under the following conditions.
+The following definitions shall have the same meaning regardless of whether they appear in singular or in plural.
+</p>
+<h2 class="title is-2">Definitions</h2>
+<p>
+For the purposes of these Terms of Service:
+<ul>
+<li><b>You</b> means the individual using Our Service</li>
+<li><b>DirectDemocracy</b> (referred to as either "We", "Us" or "Our" in this Agreement) refers to the DirectDemocracy Association, Chemin des vignes 7, CH-1124 Gollion, Swizerland.</li>
+<li><b>Service</b> (referred to as either "Donation" in this Agreement) refers to the the donation service We provide to You.</li>  
+<li><b>Third Party Payment Service</b> refers to payment service used by the Us.</li>
+<li><b>Personal Data</b> is any information that relates to an identified or identifiable individual.</li>
+</ul>
+</p>
+<h2 class="title is-2">Collecting and Using Your Personal Data</h2>
+
+<h3 class="title is-3">Types of Data Collected</h3>
+
+<h4 class="title is-4">Personal Data</h4>
+<p>
+While donating to the Us, We may ask You to provide Us with certain personally identifiable information that can be used to identify You.
+Personally identifiable information may include, but is not limited to:
+<ul>
+<li>Family name</li>
+<li>Given names</li>
+<li>Organization name</li>
+<li>E-mail address</li>
+<li>Amounts paid</li>
+<li>Currency used for the payments</li>
+<li>Comments</li>
+</ul>
+You may also provide Our Third Party Payment Services with additional information, such as:
+<ul>
+<li>Credit card information</li>
+<li>Other payment information</li>
+</ul>
+</p>
+<h4 class="title is-4">Usage Data</h4>
+<p>
+Usage Data is collected automatically by Third Party Payment Services when donating to Us.
+We strongly recommend that you review the Terms of Service of Our Third Party Payment Services to understand how your Personal Data will be used, transfered, disclosed and stored securely.
+</p>
+<h3 class="title is-3">Retention, Use, Transfer, Disclosure and Security of Your Personal Data and Usage Data</h3>
+<p>Your Personal Data is stored securely in our databases and won't be transferred or disclosed to any third party without your explicit consent.</p>
+<p>You may request Us anytime to delete your Personal Data from our databases.</p>
+<p>We may use your Personal Data to create anonymized statistical data which could be used for Our own purposes.</p>
+<p>We may occasionally send you some e-mail to inform you about the receipt of your payment, changes in these Terms of Services and other important information.</p>
+<p>We will never send you unsollicited advertising.</p>
+
+<h2 class="title is-2">Children's Privacy</h2>
+<p>
+Our Service does not address anyone under the age of 13.
+If You are a parent or guardian and You are aware that Your child has provided Personal Data to Us, please contact Us immediately.
+</p>
+<h2 class="title is-2">Links to Other Websites</h2>
+<p>
+Our Service may contain links to other websites that are not operated by Us.
+We strongly advise You to review the Privacy Policy of every site You visit.
+We have no control over and assume no responsibility for the content, privacy policies or practices of any linked website.
+</p>
+<h2 class="title is-2">Changes to these Terms of Service</h2>
+<p>
+We may update our Terms of Service from time to time. We will post the new Terms of Service on this page.
+We will update the "Last updated" date at the top of these Terms of Service.
+You are advised to review these Terms of Service periodically for any changes.
+Changes to these Terms of Service are effective when they are posted on this page.
+</p>
+<h2 class="title is-2">Contact Us</h2>
+<p>
+If you have any questions about these Terms of Service, You can contact us by email at <a href="mailto:info@direcdemocracy.vote">info@direcdemocracy.vote</a>.
+</p>
+</section>
+</div>
+</body></html>
