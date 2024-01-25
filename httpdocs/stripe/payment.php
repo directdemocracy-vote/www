ccc53b3c0d8a8166a0369fdd6757971bfdec833a
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-01-25 11:17:20 +0100

    Create payment.php

---
+++ httpdocs/stripe/payment.php
@@ -0,0 +1,13 @@
+<?php
+require_once '../../php/database.php';
+
+header("Content-Type: application/json");
+
+$key = $mysqli->escape_string($_GET['key']);
+$result = $mysqli->query("SELECT * FROM payment WHERE key='$key'") or die(mysqli->error);
+$payment = $result->fetch_assoc();
+if ($payment)
+  die(json_encode($payment, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+else
+  die('"error":"Payment not found"');
+?>
