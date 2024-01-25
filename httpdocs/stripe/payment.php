15cf5e9bfb23e17def6eb9f6bda0d6f3cca1ddbb
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-01-25 11:25:51 +0100

    Update payment.php

--- httpdocs/stripe/payment.php
+++ httpdocs/stripe/payment.php
@@ -7,7 +7,7 @@ $key = $mysqli->escape_string($_GET['key']);
 $result = $mysqli->query("SELECT * FROM payment WHERE key='$key'") or die(mysqli->error);
 $payment = $result->fetch_assoc();
 if ($payment)
-  die(json_encode($payment, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+  die(json_encode($payment, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
 else
   die('"error":"Payment not found"');
 ?>
