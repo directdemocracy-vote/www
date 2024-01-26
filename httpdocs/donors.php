f23daa093db635fb20f622a5fd0b2998a4cd5710
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-01-26 08:52:50 +0100

    Update donors.php

--- httpdocs/donors.php
+++ httpdocs/donors.php
@@ -2,13 +2,15 @@
 
 require_once '../php/database.php';
 header("Content-Type: application/json");
-$result = $mysqli->query("SELECT givenNames, familyName, frequency, currency, amount, comment, paid, displayGivenNames, hideAmount, country FROM payment WHERE display=1 AND paid!='0000-00-00 00:00:00' ORDER BY paid") or die($mysqli->error);
+$result = $mysqli->query("SELECT givenNames, familyName, organization, frequency, currency, amount, comment, paid, displayGivenNames, hideAmount, country FROM payment WHERE display=1 AND paid!='0000-00-00 00:00:00' ORDER BY paid") or die($mysqli->error);
 $payments = [];
 while($payment = $result->fetch_assoc()) {
   if ($payment['displayGivenNames'] == 1)
     $payment['familyName'] = '';
   if ($payment['hideAmount'] == 1)
     $payment['amount'] = 0;
+  unset($payment['displayGivenNames']);
+  unset($payment['hideAmount']);
   $payments[] = $payment;
 }
 die(json_encode($payments, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
