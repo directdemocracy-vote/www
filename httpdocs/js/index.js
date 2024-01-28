2d4b236f0f9b36113535f0e6d9cc6bce09ee271c
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-01-28 11:04:45 +0100

    Update index.js

--- httpdocs/js/index.js
+++ httpdocs/js/index.js
@@ -39,8 +39,6 @@ document.addEventListener('DOMContentLoaded', () => {
   translator.onready = function() {
     const language = document.getElementById('language');
     const dropdown = document.getElementById('language-dropdown');
-    function getFlag(language) {
-    }
     fetch('../i18n/flags.json')
       .then((r) => r.json())
       .then((content) => {
@@ -56,7 +54,7 @@ document.addEventListener('DOMContentLoaded', () => {
           translator.language = language;
           document.getElementById(`language-${previous}`).classList.remove('is-disabled');
           document.getElementById(`language-${language}`).classList.add('is-disabled');
-          document.getElementById('language').innerHTML = '<img src="/images/flags/' + getFlag(language) + '.svg" width="28">';
+          document.getElementById('language').innerHTML = '<img src="/images/flags/' + language + '.svg" width="28">';
         }
         flags = content;
         let first = true;
@@ -65,7 +63,7 @@ document.addEventListener('DOMContentLoaded', () => {
           a.classList.add('navbar-item');
           if (first) {
             a.classList.add('is-disabled');
-            language.innerHTML = '<img src="/images/flags/' + getFlag(country) + '.svg" width="28">';
+            language.innerHTML = '<img src="/images/flags/' + flag + '.svg" width="28">';
             first = false;
           }
           a.setAttribute('id', `language-${country}`);
