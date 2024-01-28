549525c38e8bff721fedb29033606cd65783b8e0
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-01-28 10:40:52 +0100

    Update index.js

--- httpdocs/js/index.js
+++ httpdocs/js/index.js
@@ -165,12 +165,13 @@ document.addEventListener('DOMContentLoaded', () => {
           img.width = '32px';
           img.title = payment.country;
           img.align = 'top';
-          // td.textContent = getFlagEmoji(payment.country);
+          /* td.textContent = getFlagEmoji(payment.country);
           td.title = payment.country;
           td.style.fontStyle = 'normal';
           td.style.textAlign = 'center';
           td.style.padding = '0';
           td.style.fontSize = '160%';
+          */
         }
       });
   }
