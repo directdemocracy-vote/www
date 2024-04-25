5d8401a64a7c2766efa994962d182798bffa80f1
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-04-25 08:32:32 +0200

    Create index.php

---
+++ httpdocs/democracy_day/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Terms of Service</title>
+  <link rel="icon" href="/favicon.svg" type="image/svg+xml" sizes="any" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css" />
+  <link rel="stylesheet" href="https://directdemocracy.vote/css/directdemocracy.css" />
+</head>
+<body>
+<div class="content">
+<section class="section">
+
+<h1 class="title is-1">Democracy Day &mdash; directdemocracy.vote</h1>
+
+<p>Le dimanche 15 septembre 2024, c'est la journée mondiale de la démocratie. Ce jour là, nous organisons le lancement de notre projet de démocratie directe mondiale.
+</p>
+
+<h2 class="title is-2">Lieu</h2>
+
+EPFL Innovation Park
+
+<h2 class="title is-2">Programme</h2>
+
+14h00: Présentation<br>
+14h30: Questions / Réponses<br>
+15h00: Installation de l'app par les participants, mini-débat, et premier vote democratique mondial.
+
+<h2 class="title is-2">Plus d'information</h2>
+
+<p>
+  Davantage d'informations seront publiées sur ce site d'ici au 15 septembre 2024.
+  En attendant, vous pouvez visiter notre <a href="https://directdemocracy.vote">site web</a>.
+  Écrivez-nous à <a href="mailto:info@directdemocracy.vote">info@directdemocracy.vote</a> pour être tenu informé de cet évènement.</p>
+</section>
+</div>
+</body></html>
