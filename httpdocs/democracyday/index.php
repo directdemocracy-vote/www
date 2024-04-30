6b8b4935a74dbcfd31f950d2dd9eabb455f8edd1
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-04-30 08:15:15 +0200

    Update index.php

--- httpdocs/democracyday/index.php
+++ httpdocs/democracyday/index.php
@@ -14,32 +14,43 @@
 
 <h1 class="title is-1">Democracy Day</h1>
 
-<p>Le dimanche 15 septembre 2024, c'est la <a href="https://www.un.org/observances/democracy-day" target="_blank">journée mondiale de la démocratie</a>. Ce jour là, nous organisons le lancement de notre projet de démocratie directe mondiale.
+<h2 class="title is-2">Quoi ?</h2>
+<p>
+  À l'occasion de la <a href="https://www.un.org/observances/democracy-day" target="_blank">journée mondiale de la démocratie</a>, nous organisons le lancement de notre projet de démocratie directe mondiale.
 </p>
 
-<h2 class="title is-2">Lieu</h2>
+<h2 class="title is-2">Quand ?</h2>
+<p>
+  Dimanche 15 septembre 2004.
+</p>
+  
+<h2 class="title is-2">Où ?</h2>
+<p>
+  À Lausanne (l'adresse exacte sera publiée ici prochainment).
+</p>
 
-EPFL Innovation Park<br>
-CH-1015 Lausanne
-
-<h2 class="title is-2">Programme</h2>
-
+<h2 class="title is-2">Par ici le programme !</h2>
 <h3 class="title is-3">14h00: Présentation</h3>
-<p><b>Démarrons ensemble une démocratie directe mondiale le 15 septembre 2024 !</b></p>
-<p>Notre planète a urgemment besoin d'une gouvernance réellement démocratique.
-Devenez les premiers citoyens d'une application mobile révolutionnaire qui ambitionne de changer monde.</p>
-
+<p>
+  <b>Démarrons ensemble une démocratie directe mondiale le 15 septembre 2024 !</b>
+</p>
+<p>
+  Notre planète a urgemment besoin d'une gouvernance réellement démocratique.
+  Devenez les premiers citoyens d'une application mobile révolutionnaire qui ambitionne de changer monde.
+</p>
 <h3 class="title is-3">14h30: Questions / Réponses</h3>
-
 <h3 class="title is-3">15h00: Atelier pratique</h3>
-<p>Installation de l'app par les participants, mini-débat et participation au premier vote democratique mondial.</p>
+<p>
+  Installation de l'app par les participants, mini-débat et participation au premier vote democratique mondial.
+</p>
 
 <h2 class="title is-2">Plus d'information</h2>
-
 <p>
-  Davantage d'informations seront publiées sur ce site d'ici au 15 septembre 2024.
+  Davantage d'informations seront publiées sur ce site prochainement.
   En attendant, vous pouvez visiter notre <a href="https://directdemocracy.vote">site web</a>.
-  Écrivez-nous à <a href="mailto:info@directdemocracy.vote">info@directdemocracy.vote</a> pour être tenu informé de cet évènement.</p>
+  Écrivez-nous à <a href="mailto:info@directdemocracy.vote">info@directdemocracy.vote</a> pour être tenu informé de cet évènement.
+</p>
+
 </section>
 </div>
 </body></html>
